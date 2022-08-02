604bf4b742466f07ced3053ff6825e52e537118e
AhmedAlaa123 <mm1869695@gmail.com>
2022-08-02 19:19:20 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+using Microsoft.EntityFrameworkCore;
+using noone.Models;
+
+namespace noone
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            // add dbcontext to service
+            //get connection string
+            string connectionString = builder.Configuration.GetConnectionString("Ahmed Alaa");
+            builder.Services.AddDbContext<NoonEntities>(optionsBuilde =>
+            {
+                optionsBuilde.UseSqlServer(connectionString);
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ Reposatories/IReposatory.cs
@@ -0,0 +1,11 @@
+﻿namespace noone.Reposatories
+{
+    public interface IReposatory<T>
+    {
+        bool Insert(T item);
+        bool Delete(T item);
+        bool Update(Guid Id);
+        T GetById(Guid Id);
+        ICollection<T> GetAll();
+    }
+}
