69d5c99b9220ffb68607936f2e719af5ce3874b6
Khaled 15259687 <Khaled.15259687@compit.aun.edu.eg>
2022-08-03 16:01:33 +0200

    info dto

--- ApplicationDTO/SubCategoryDTO/SubCategoryInfoDTO.cs
+++ ApplicationDTO/SubCategoryDTO/SubCategoryInfoDTO.cs
@@ -1,6 +1,18 @@
-﻿namespace noone.ApplicationDTO.SubCategoryDto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace noone.ApplicationDTO.SubCategoryDto
 {
     public class SubCategoryInfoDTO
     {
+        
+        public Guid SubCategoryId { get; set; }
+
+        [Required(ErrorMessage = "من فضلك ادخل اسم الفئة")]
+        [MinLength(3, ErrorMessage = "يجب ان يكون الاسم اكثر من حرفين")]
+        public string SubCategoryName { get; set; }
+        public string? Image { get; set; }
+        public Guid Category_Id { get; set; }
+
+
     }
 }
