dd50e4709e89f3bbc35df902cbb20cae6b83ca58
ebrahemahmed5056 <ebrahem123bazedy@gmail.com>
2022-08-05 01:00:14 +0200

    .

---
+++ DTO/categoryDTO/categoryDTO.cs
@@ -0,0 +1,14 @@
+﻿using noone.CustomAttributes;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace noone.DTO.categoryDTO
+{
+    public class AddcategoryDTO
+    {
+        
+
+        [Required, MinLength(3), uniqeCategoryName]
+        public string Name { get; set; }
+    }
+}
