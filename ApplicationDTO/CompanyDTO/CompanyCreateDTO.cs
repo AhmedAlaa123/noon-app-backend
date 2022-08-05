594d624d0b353601c62198104b489a514acee092
NANOSOFT <mohamedahmeds951@gmail.com>
2022-08-06 01:41:26 +0200

    uniqename

--- ApplicationDTO/CompanyDTO/CompanyCreateDTO.cs
+++ ApplicationDTO/CompanyDTO/CompanyCreateDTO.cs
@@ -1,10 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using noone.CustomAttributes;
 
 namespace noone.ApplicationDTO.CompanyDTO
 {
     public class CompanyCreateDTO
     {
-        [Required(ErrorMessage = "اسم الشركه مطلوب"), MinLength(3, ErrorMessage = "اقل عدد حروف 3")]
+        [Required(ErrorMessage = "اسم الشركه مطلوب"), MinLength(3, ErrorMessage = "اقل عدد حروف 3"),UniqeCompanyName]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "رقم الهاتف مطلوب"), StringLength(11, ErrorMessage = "رقم الهاتف يجب ان يكن 11 رقم"), RegularExpression("^[0-9]{11}$")]
---
+++ CustomAttributes/UniqeCompanyName.cs
@@ -0,0 +1,25 @@
+﻿using noone.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace noone.CustomAttributes
+{
+    public class UniqeCompanyName:ValidationAttribute
+    {
+        public NoonEntities context = new NoonEntities();
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                Company comp = this.context.Companies.FirstOrDefault(c => c.Name == (value.ToString().Trim()));
+                if (comp == null)
+                    return ValidationResult.Success;
+
+                return new ValidationResult("اسم الشركة موجود يجب ان يكون الاسم مميزا ");
+            }
+            return new ValidationResult("ادخل اسم الشركة");
+
+        }
+    }
+}
+
